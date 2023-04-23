41ed551fb5193c4d5e7227116bfc8b2c86723c5f
unknown <brhouma85@gmail.com>
2023-04-23 16:54:06 -0700

    finished questions array

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: '',
-        name: '',
-        message: '',
+        type: 'input',
+        name: 'title',
+        message: 'Name your Project.',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe this project.',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select license for this project.',
+        choices: ["MIT", "APACHE2.0", "Boost1.0", 'MPL2.0', "BSD2", "BSD3", "none"],
+    },
+    {
+        type: 'input',
+        name: 'require',
+        message: 'List dependencies here.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'State the languages or technologies associated with this project.',
     },
 
 ];
