8181c24679550da3217cc062bbf67fcc7f8b9d3e
unknown <brhouma85@gmail.com>
2023-04-23 15:53:44 -0700

    working on functions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
-const fs =require("fs");
+const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-const generateMarkdown = require('.')
+const generateMarkdown = require('./generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
@@ -17,9 +17,16 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName),);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponse) => {
+        console.log("Welcome to my README!!");
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponse }));
+    });
+}
 
 init();
